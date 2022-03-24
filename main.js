11293eb5027337f06b0d80ab4c3655e677583e2e
malkieg <48064960+malkieg@users.noreply.github.com>
2022-03-24 17:40:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+//Run jQuery after the document is fully loaded.
+$(document).ready(
+    //The function that does the stuff.
+    function () {
+
+        let requestedShow='';
+        $('#submit-show').bind('click', function(e)
+         {
+             requestedShow='';
+
+
+             e.preventDefault();
+             requestedShow=document.getElementById('requested').value;
+             requestedShow = requestedShow.replace(/\s/g, "+");
+            console.log(requestedShow);
+
+
+        //Make the AJAX call
+        $.ajax('https://api.tvmaze.com/singlesearch/shows?q=' + requestedShow + '&embed=episodes', {
+            method: "GET",
+            dataType: "json"
+        })
+
+
+        //After the data comes back, use this function
+            .done(
+                function (data) {
+                    //Add the name
+                        $("name").html("");
+                        $('#name').html(data.name);
+                    $('#episodeList').html("");
+
+                    // $('#name').bind('click', function (e) {
+                    //     e.preventDefault();
+
+                    //Add the episodes
+
+                    data._embedded.episodes.forEach(function (episode) {
+                        $('#episodeList').append('<tr>'+
+                            '<td>' + episode.season + '</td>' +
+                            '<td>' + episode.number + '</td>' +
+                            '<td>' + episode.name + '</td>' +
+                            '<td>' + episode.summary + '</td>' +
+                            +' </tr>')
+            })
+                              //})
+        })
+             }
+         );
+    }
+)
